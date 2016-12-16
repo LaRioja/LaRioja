2179350a6ddff77e97f84650d6b16cadbec68073
crlaenci <claencina@organizacion.lan>
2016-12-16 09:27:32 +0100

    hiernateUtil

---
+++ src/java/main/manages/ManageHospital.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.manages;
+
+import java.util.ArrayList;
+import java.util.List;
+import main.model.Hospital;
+import main.persistence.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author claencina
+ */
+public class ManageHospital {
+
+    public static int save(Hospital hospital) {
+        SessionFactory factory = HibernateUtil.getSessionFactory();
+        Session sess = factory.openSession();
+        Transaction tx = null;
+        Integer ok = -1;
+        try {
+            tx = sess.beginTransaction();
+            ok = (Integer) sess.save(hospital);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+        } finally {
+            sess.close();
+        }
+        return ok;
+    }
+
+    public static Object update(Hospital hospital) {
+        SessionFactory factory = HibernateUtil.getSessionFactory();
+        Session sess = factory.openSession();
+        Transaction tx = null;
+        Object ok = null;
+        try {
+            tx = sess.beginTransaction();
+            ok = sess.merge(hospital);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+        } finally {
+            sess.close();
+        }
+        return ok;
+    }
+
+    public static void delete(Hospital hospital) {
+        SessionFactory factory = HibernateUtil.getSessionFactory();
+        Session sess = factory.openSession();
+        Transaction tx = null;
+        try {
+            tx = sess.beginTransaction();
+            sess.delete(hospital);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+        } finally {
+            sess.close();
+        }
+    }
+
+    public static List<Hospital> list() {
+        SessionFactory factory = HibernateUtil.getSessionFactory();
+        Session sess = factory.openSession();
+        Transaction tx = null;
+        List<Hospital> hospital = new ArrayList();
+        try {
+            tx = sess.beginTransaction();
+            hospital = sess.createQuery("from hospital").list();
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+        } finally {
+            sess.close();
+        }
+        return hospital;
+    }
+    
+    public static Hospital read(int id) {
+        SessionFactory factory = HibernateUtil.getSessionFactory();
+        Session sess = factory.openSession();
+        Transaction tx = null;
+        Hospital hospital = new Hospital();
+        try {
+            tx = sess.beginTransaction();
+            hospital = (Hospital) sess.get(Hospital.class, id);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+        } finally {
+            sess.close();
+        }
+        return hospital;
+    }
+}
