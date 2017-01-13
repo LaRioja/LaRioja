8ce699809961fc770a908d75149d2ca043240d0f
pamayor <pamayor@unirioja.es>
2017-01-13 09:34:51 +0100

    delete webdav

--- src/java/servlets/EliminarContenidoExtra.java
+++ src/java/servlets/EliminarContenidoExtra.java
@@ -5,11 +5,9 @@
  */
 package servlets;
 
-import java.io.File;
+import com.github.sardine.Sardine;
+import com.github.sardine.SardineFactory;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -22,31 +20,21 @@ public class EliminarContenidoExtra extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        String id = request.getParameter("id");
-        
-        String ruta = "/contenidoExtra";
-        String path = request.getRealPath(ruta);
-        boolean borrado = false;
-        boolean encontrado = false;
-
-        File f = new File(path);
-        if (f.exists() && f.isDirectory()) {
-            File[] ficheros = f.listFiles();
-            for (int i = 0; i < ficheros.length; i++) {
-                if(ficheros[i].getName().compareTo(id)==0){
-                    borrado = ficheros[i].delete();
-                    encontrado = true;
-                }
+        try {
+            String id = request.getParameter("id");
+            Sardine sardine = SardineFactory.begin("webdavuser", "password");
+            String url = "http://" + request.getServerName() + ":" + request.getServerPort() + request.getServletContext().getContextPath() + "/contenidos/contenidoExtra/" + id;
+            if (sardine.exists(url)) {
+                sardine.delete(url);
+                response.sendRedirect("ContenidoExtra?msg=okDel");
+                return;
+            } else {
+                request.setAttribute("error_borrar", "No es posible eliminar el fichero");
             }
-        }
-        
-        if(encontrado && borrado){
-            response.sendRedirect("ContenidoExtra?msg=okDel");
-        }else if(!borrado){
+        } catch (Exception e) {
             request.setAttribute("error_borrar", "No es posible eliminar el fichero");
-            RequestDispatcher rd = request.getRequestDispatcher("contenidoExtra.jsp");
-            rd.forward(request, response);
         }
+        RequestDispatcher rd = request.getRequestDispatcher("contenidoExtra.jsp");
+        rd.forward(request, response);
     }
-
 }
